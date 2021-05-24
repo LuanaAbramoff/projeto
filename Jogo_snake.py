d395e2f517dc655bac1347cb8e8743d593292728
Douglas <douglaspablo2018@gmail.com>
2021-05-24 11:59:41 -0300

    Abordando inicialização do jogo (Janela e Nome)

---
+++ Jogo_snake.py
@@ -0,0 +1,11 @@
+#=================== Inicialização ===============
+import pygame
+import random
+
+pygame.init()
+
+# Criando guia princípal e nome do jogo
+janela = pygame.display.set_mode((480,320))
+pygame.display.set_caption('Snake Retrô')
+
+pygame.quit()
