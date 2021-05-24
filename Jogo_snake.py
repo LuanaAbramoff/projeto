87d120a74786afb4638feef74eda993af70de54a
Douglas <douglaspablo2018@gmail.com>
2021-05-24 15:09:58 -0300

    Adiciona imagem

--- Jogo_snake.py
+++ Jogo_snake.py
@@ -8,11 +8,15 @@ pygame.init()
 janela = pygame.display.set_mode((480,320))
 pygame.display.set_caption('Snake Retrô')
 
+fundo_jogo = pygame.image.load('assets/Fundo.jpeg').convert()
+
 game= True
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game= False
-    janela.fill((0,255,0))
+            game = False
+    janela.fill((0,0,0))
+    janela.blit(fundo_jogo, (0, 0))
     pygame.display.update()
+
 pygame.quit()
