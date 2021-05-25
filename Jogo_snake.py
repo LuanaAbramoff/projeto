74d62c58b5e8b0de126558a632324e284f713910
riadaltinawi <81169967+riadaltinawi@users.noreply.github.com>
2021-05-25 02:21:26 -0300

    arrumando erro

--- Jogo_snake.py
+++ Jogo_snake.py
@@ -26,13 +26,7 @@ cinzaClaro=(220,220,220)
 fps = pygame.time.Clock()
 janela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption('Snake Retrô')
-class textos:
-    def __init__(self, msg, cor, tam):
-            self.font = pygame.font.SysFont(None, tam)
-            self.texto = self.font.render(msg, True, cor)
 
-    def mostra(self, x, y):
-            janela.blit(self.texto, [x, y])
 
 
 # Musicas e imagens 
