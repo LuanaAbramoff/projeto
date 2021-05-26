8f5e341381951d403ff99021ff1ca3205d1479cc
riadaltinawi <81169967+riadaltinawi@users.noreply.github.com>
2021-05-26 10:28:56 -0300

    .

--- Jogo_snake.py
+++ Jogo_snake.py
@@ -1,7 +1,6 @@
 #=================== Inicialização ===============
-from Classes.Cobra import Cobra
 import pygame
-import random
+from random import *
 import os
 
 
@@ -44,6 +43,43 @@ maca= pygame.image.load('assets/Imagens/Untitled-1.png').convert()
 maca_pequena =pygame.transform.scale(maca, (maca_w, maca_h))
 maca_x = random.randint(0,480)
 maca_y = random.randint(0,320)
+
+class Cobra:
+    def __init__(self):
+        self.x = randint(0,(largura-tamanho)/10)*10
+        self.y = randint(0,(altura-tamanho)/10)*10
+        self.velocidade_em_x = 0
+        self.velocidade_em_y = 0
+        self.cobra_xy = []
+        self.cobra_comprimento = 1
+        self.cobra_inicial = []
+        self.pontuacao = 0
+        self.fimdejogo = False
+    def movimentos_possiveis(self):
+        self.cobra_0 = [self.x,self.y]
+        self.cobra_xy.append(self.cobra_inicial) 
+    def crescimento_cobra(self):
+        self.cobra_comprimento += 1
+    def imagem_cobra(self):
+        for XY in self.cobra_xy:
+            pygame.draw.rect(fundo_jogo,branco,[XY[0],XY[1],tamanho,tamanho])
+    def resto(self):
+        if len(self.cobra_xy) > self.cobra_comp:
+            del self.cobra_xy[0]
+    def morte(self):
+        if any(Bloco == self.cobra_0 for Bloco in self.cobra_xy[:-1]):
+            self.fimdejogo = True
+
+    def reinicio(self):
+            self.x = randint(0,(largura-tamanho)/10)*10
+            self.y = randint(0,(altura-tamanho)/10)*10
+            self.vel_x = 0
+            self.vel_y = 0
+            self.cobra_xy = []
+            self.cobra_comp = 1
+            self.cobra_0 = []
+            self.pontos = 0
+            self.fimdejogo = False 
 
 
 class textos:
