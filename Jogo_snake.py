d06886b26bbb8a07e3d58359043ee22937709d1e
Douglas <douglaspablo2018@gmail.com>
2021-05-26 00:24:38 -0300

    Arrumando dificuldade do jogo

--- Jogo_snake.py
+++ Jogo_snake.py
@@ -1,11 +1,10 @@
 #=================== Inicialização ===============
 #from Classes.maca import macav
 from Classes.Cobra import Cobra
-from Classes.textos import textos
 import pygame
 import random
 import os
-from Classes import *
+
 
 pygame.init()
 
@@ -37,14 +36,26 @@ pygame.mixer.music.set_volume(0.4)
 pygame.mixer.music.play(-1)
 fundo_jogo = pygame.image.load('assets/Imagens/Fundo.jpeg').convert()
 
-#maça aparecendo em lugar aleatorio 
-#maca_w= 15
-#maca_h= 15
 
-#maca= pygame.image.load('assets/Imagens/Untitled-1.png').convert()
-#maca_pequena=pygame.transform.scale(maca, (maca_w, maca_h))
-#maca_x = random.randint(0,480)
-#maca_y = random.randint(0,320)
+#maça aparecendo em lugar aleatorio 
+maca_w= 15
+maca_h= 15
+
+maca= pygame.image.load('assets/Imagens/Untitled-1.png').convert()
+maca_pequena =pygame.transform.scale(maca, (maca_w, maca_h))
+maca_x = random.randint(0,480)
+maca_y = random.randint(0,320)
+
+
+class textos:
+    def __init__(self, msg, cor, tam):
+            self.font = pygame.font.SysFont(None, tam)
+            self.texto = self.font.render(msg, True, cor)
+
+    def mostra(self, x, y):
+            janela.blit(self.texto, [x, y])
+
+
 
 game = True
 
@@ -55,12 +66,8 @@ while game:
 
     janela.fill((0,0,0))
     janela.blit(fundo_jogo, (0, 0))
-    #janela.blit(maca_pequena, (maca_x, maca_y))
+    janela.blit(maca_pequena, (maca_x, maca_y))
     pygame.display.update()
-
-
-
-
 
 
 # encerramento do jogo
@@ -71,33 +78,33 @@ inicio_do_jogo = True
 cob = Cobra()
 #mac = macav()
 palavra = textos("Game Over", vermelho, 37)
-palavra2= textos("Pontuação: " , branco, 27)
+palavra2 = textos("Pontuação: " , branco, 27)
 palavra3 = textos("aperte Q",branco,27)
 palavra4 = textos("Deseja continuar?",branco,27)
-palavra5= textos("Snake Retrô",branco,35)
-palavra6=textos("Ecolha o nível de dificuldade do jogo",vermelho,27)
+palavra5 = textos("Snake Retrô",branco,35)
+palavra6 = textos("Escolha o nível de dificuldade do jogo",vermelho,27)
 
 
 while inicio_do_jogo:
     janela.blit(fundo_jogo,(0,0)) 
-    palavra5.mostra(80,130)
-    palavra6.mostra(60,160)
+    palavra5.mostra(0,0)
+    palavra6.mostra(80,30)
     
     # escolha da dificuldade 
-    pygame.draw.rect(fundo_jogo, cinzaClaro, [43, 118, 139, 31])
-    pygame.draw.rect(fundo_jogo, preto, [45, 120, 135, 27])
+    pygame.draw.rect(janela, cinzaClaro, [43, 118, 139, 31])
+    pygame.draw.rect(janela, preto, [45, 120, 135, 27])
     facil = textos("fácil(1)", branco, 30)
-    facil.mostra(50, 125)
+    facil.mostra(80, 125)
 
-    pygame.draw.rect(fundo_jogo, cinzaClaro, [70, 118, 139, 50])
-    pygame.draw.rect(fundo_jogo, preto, [75, 120, 135, 60])
+    pygame.draw.rect(janela, cinzaClaro, [43, 168, 139, 31])
+    pygame.draw.rect(janela, preto, [45, 170, 135, 27])
     medio = textos("médio(2)", branco, 30)
-    medio.mostra(60, 125)
+    medio.mostra(73, 175)
 
-    pygame.draw.rect(fundo_jogo, cinzaClaro, [80, 118, 139, 70])
-    pygame.draw.rect(fundo_jogo, vermelho, [83, 120, 135, 75])
-    dificil = textos("díficil(3)", branco, 30)
-    dificil.mostra(70, 125)
+    pygame.draw.rect(janela, cinzaClaro, [43, 218, 139, 31])
+    pygame.draw.rect(janela, preto, [45, 220, 135, 27])
+    dificil = textos("díficil(3)", vermelho, 30)
+    dificil.mostra(74, 225)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -129,7 +136,7 @@ while inicio_do_jogo:
                     if event.key == pygame.K_q:
                         fim_do_jogo=False
                         jogo=True
-                        cob.reinicio()
+                        cob.recomeco()
                         #falta atualizar os pontos
                         pygame.mixer.music.load(musica_inicio)
                         pygame.mixer.music.set_volume(0.5)
