328432a22b3f56d7409cfe7353130bcb995ad3db
Douglas <douglaspablo2018@gmail.com>
2021-05-29 19:46:06 -0300

    Corringindo maçã dourada

--- Recomeço.py
+++ Recomeço.py
@@ -60,7 +60,7 @@ class Maca_dourada:
         self.pos2_y = random.randint(0,(altura-tamanho)/10)*10
         self.w = 14
         self.h = 14
-        self.img = pygame.image.load('assets/Imagens/gold_mine.png').convert()
+        self.img = pygame.image.load('assets/Imagens/Golden_Apple_JE2_BE2.png').convert()
         self.convercao = pygame.transform.scale(self.img, (self.w, self.h))
 
 # Classe da criação e movimentação da cobra
